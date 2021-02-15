311b757c421b5df831e3afb56e68a5c476a9f5c8
will buttinger <will@cern.ch>
2021-02-15 11:19:07 +0000

    initial commit

---
+++ TTreeIterator/src/LinkDef.h
@@ -0,0 +1,5 @@
+// do we need an ifdef __MAKECINT__ or something similar?
+
+#include "TTreeIterator/RooFitResultTree.h"
+#pragma link C++ class RooFitResultTree+;
+#pragma link C++ class RooDataTree+;
