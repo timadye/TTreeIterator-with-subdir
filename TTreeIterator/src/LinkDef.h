f10c2172711f09accf79877b8dd616f590b75f74
Tim Adye <Tim.Adye@cern.ch>
2021-03-17 20:59:42 +0000

    start implementation of RooFitTaskTree and add use to scanLH.py

--- TTreeIterator/src/LinkDef.h
+++ TTreeIterator/src/LinkDef.h
@@ -1,5 +1,7 @@
 // do we need an ifdef __MAKECINT__ or something similar?
 
 #include "TTreeIterator/RooFitResultTree.h"
+#include "TTreeIterator/RooFitTaskTree.h"
 #pragma link C++ class RooFitResultTree+;
 #pragma link C++ class RooDataTree+;
+#pragma link C++ class RooFitTaskTree+;
