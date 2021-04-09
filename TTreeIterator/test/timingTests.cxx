821bbda6dd9ab2a607e91011d6aaaeae2e2b7ac4
Tim Adye <Tim.Adye@cern.ch>
2021-04-09 23:06:27 +0100

    allow variable number of branches with single-double test

--- TTreeIterator/test/timingTests.cxx
+++ TTreeIterator/test/timingTests.cxx
@@ -21,9 +21,10 @@
 #define DO_GET
 #define FULL_CHECKS
 
-const Long64_t nfill1 = 100000;
+const Long64_t nfill1 = 500000;
 const Long64_t nfill2 = 100000;
 const Long64_t nfill3 = 100000;
+constexpr size_t nx1 = 20;
 constexpr size_t nx2 = 100;
 constexpr size_t nx3 = 100;
 const double vinit = 42.3;  // fill each element with a different value starting from here
@@ -78,19 +79,14 @@ TEST(timingTests1, FillIter) {
   TFile f ("test_timing1.root", "recreate");
   ASSERT_FALSE(f.IsZombie()) << "no file";
 
+  std::vector<std::string> bnames;
+  bnames.reserve(nx1);
+  for (int i=0; i<nx1; i++) bnames.push_back (Form("x%03d",i));
+
   TTreeIterator titer ("test");
   double v = vinit;
   for (auto& e : titer.FillEntries(nfill1)) {
-    e["x0"] = v++;
-    e["x1"] = v++;
-    e["x2"] = v++;
-    e["x3"] = v++;
-    e["x4"] = v++;
-    e["x5"] = v++;
-    e["x6"] = v++;
-    e["x7"] = v++;
-    e["x8"] = v++;
-    e["x9"] = v++;
+    for (auto& b : bnames) e[b.c_str()] = v++;
   }
   Int_t nbranches = ShowBranches (f, titer.GetTree(), branch_type1, "filled");
   EXPECT_FLOAT_EQ (vinit+double(nbranches*nfill1), v);
@@ -104,33 +100,20 @@ TEST(timingTests1, GetIter) {
   ASSERT_TRUE(titer.GetTree()) << "no tree";
   EXPECT_EQ(titer.GetEntries(), nfill1);
   Int_t nbranches = ShowBranches (f, titer.GetTree(), branch_type1);
-  EXPECT_EQ(nbranches, 10);
+  EXPECT_EQ(nbranches, nx1);
+
+  std::vector<std::string> bnames;
+  bnames.reserve(nx1);
+  for (int i=0; i<nx1; i++) bnames.push_back (Form("x%03d",i));
 
   double v = vinit;
   for (auto& e : titer) {
-    double
-      x0=e["x0"],
-      x1=e["x1"],
-      x2=e["x2"],
-      x3=e["x3"],
-      x4=e["x4"],
-      x5=e["x5"],
-      x6=e["x6"],
-      x7=e["x7"],
-      x8=e["x8"],
-      x9=e["x9"];
+    for (auto& b : bnames) {
+      double x = e[b.c_str()];
 #ifdef FULL_CHECKS
-    EXPECT_EQ (x0, v++);
-    EXPECT_EQ (x1, v++);
-    EXPECT_EQ (x2, v++);
-    EXPECT_EQ (x3, v++);
-    EXPECT_EQ (x4, v++);
-    EXPECT_EQ (x5, v++);
-    EXPECT_EQ (x6, v++);
-    EXPECT_EQ (x7, v++);
-    EXPECT_EQ (x8, v++);
-    EXPECT_EQ (x9, v++);
+      EXPECT_EQ (x, v++);
 #endif
+    }
   }
 }
 
@@ -143,28 +126,12 @@ TEST(timingTests1, FillAddr) {
   ASSERT_FALSE(f.IsZombie()) << "no file";
 
   auto tree = new TTree("test","");
-  double x0; tree->Branch("x0",&x0);
-  double x1; tree->Branch("x1",&x1);
-  double x2; tree->Branch("x2",&x2);
-  double x3; tree->Branch("x3",&x3);
-  double x4; tree->Branch("x4",&x4);
-  double x5; tree->Branch("x5",&x5);
-  double x6; tree->Branch("x6",&x6);
-  double x7; tree->Branch("x7",&x7);
-  double x8; tree->Branch("x8",&x8);
-  double x9; tree->Branch("x9",&x9);
+  std::vector<double> vals(nx1);
+  for (int i=0; i<nx1; i++) tree->Branch (Form("x%03d",i), &vals[i]);
+
   double v = vinit;
   for (Long64_t i = 0; i < nfill1; i++) {
-    x0 = v++;
-    x1 = v++;
-    x2 = v++;
-    x3 = v++;
-    x4 = v++;
-    x5 = v++;
-    x6 = v++;
-    x7 = v++;
-    x8 = v++;
-    x9 = v++;
+    for (auto& x : vals) x = v++;
     tree->Fill();
   }
   f.Write();
@@ -182,34 +149,17 @@ TEST(timingTests1, GetAddr) {
   ASSERT_TRUE(tree) << "no tree";
   EXPECT_EQ(tree->GetEntries(), nfill1);
   Int_t nbranches = ShowBranches (f, tree, branch_type1);
-  EXPECT_EQ(nbranches, 10);
+  EXPECT_EQ(nbranches, nx1);
 
-  double x0; tree->SetBranchAddress("x0",&x0);
-  double x1; tree->SetBranchAddress("x1",&x1);
-  double x2; tree->SetBranchAddress("x2",&x2);
-  double x3; tree->SetBranchAddress("x3",&x3);
-  double x4; tree->SetBranchAddress("x4",&x4);
-  double x5; tree->SetBranchAddress("x5",&x5);
-  double x6; tree->SetBranchAddress("x6",&x6);
-  double x7; tree->SetBranchAddress("x7",&x7);
-  double x8; tree->SetBranchAddress("x8",&x8);
-  double x9; tree->SetBranchAddress("x9",&x9);
+  std::vector<double> vals(nx1);
+  for (int i=0; i<nx1; i++) tree->SetBranchAddress (Form("x%03d",i), &vals[i]);
 
   double v = vinit;
   Long64_t n = tree->GetEntries();
   for (Long64_t i=0; i<n; i++) {
     tree->GetEntry(i);
 #ifdef FULL_CHECKS
-    EXPECT_EQ (x0, v++);
-    EXPECT_EQ (x1, v++);
-    EXPECT_EQ (x2, v++);
-    EXPECT_EQ (x3, v++);
-    EXPECT_EQ (x4, v++);
-    EXPECT_EQ (x5, v++);
-    EXPECT_EQ (x6, v++);
-    EXPECT_EQ (x7, v++);
-    EXPECT_EQ (x8, v++);
-    EXPECT_EQ (x9, v++);
+    for (auto& x : vals) EXPECT_EQ (x, v++);
 #endif
   }
   tree->ResetBranchAddresses();
