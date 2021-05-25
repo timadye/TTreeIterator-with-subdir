013b155301b04e3c44bd2fccbe6f7d6a2e34f576
Tim Adye <Tim.Adye@cern.ch>
2021-05-25 15:28:35 +0100

    improved timing test plotting: easier way to list input files. use TProfile

--- TTreeIterator/test/plotTimes.py
+++ TTreeIterator/test/plotTimes.py
@@ -7,7 +7,7 @@ import ROOT
 cols = [ROOT.kBlue, ROOT.kRed, ROOT.kGreen]
 
 def parseArgs():
-  parser= optparse.OptionParser(usage="%prog [OPTIONS] TIMING.CSV")
+  parser= optparse.OptionParser(usage="%prog [OPTIONS] timing1a.csv [timing1b.csv...] [ : timing2a.csv [timing2b.csv...] [ : ...]]")
   parser.add_option ("-v", "--verbose",      help="verbose running", action="count", default=0)
   parser.add_option ("-i", "--interactive",  help="ROOT interactive mode", action="store_true")
   parser.add_option ("-o", "--output",       help="plot pdf file")
@@ -27,20 +27,28 @@ def process ():
   ROOT.TH1.AddDirectory(0)
   legends = opt.legends.split(",") if opt.legends else []
   trees = []
-  for i,fspec in enumerate(reversed(args)):
-    name = "times_%d" % (len(args)-i-1)
-    tree = ROOT.TTree(name,name)
-    trees.insert(0,tree)
-    for i,f in enumerate(fspec.split(",")):
-      if not i:
-        n = tree.ReadFile(f,"",",")
-      else:
-        # skip header line
-        fs = ROOT.std.ifstream(f)
-        while chr(fs.get()) != '\n': pass
-        n = tree.ReadStream(fs,"",",")
-        del fs
-      if not n: return 1
+  add=False
+  i = 0
+  for f in args:
+    if f == ":":
+      add=False
+      i += 1
+      continue
+    if not add:
+      add=True
+      tree = ROOT.TTree ("times_%d" % i, f)
+      trees.append(tree)
+      n = tree.ReadFile(f,"",",")
+    else:
+      # skip header line
+      fs = ROOT.std.ifstream(f)
+      while True:
+        c=fs.get()
+        if c==10 or c<0: break   # stop on '\n' or end of file
+      n = tree.ReadStream(fs,"",",")
+      del fs
+    if not n: return 1
+    print ("read %d entries from file %s into tree '%s'" % (n, f, tree.GetName()))
   nt = len(trees)
   sf = 1.0 / float(nt)
   w = min (0.45, 0.55*sf)
@@ -48,21 +56,25 @@ def process ():
   ROOT.gStyle.SetTickLength(0.0)
   tree0 = trees[0]
   tree0.Draw("label","","goff")
-  h0 = tree.GetHistogram()
-  h0.SetStats(0)
-  h0.SetTitle(";;ns / double")
+  h0 = tree0.GetHistogram()
+  if not h0:
+    print ("element 'label' not found in '%s'" % tree0.GetName())
+    return 2
+  h0.Reset("M")
   ax = h0.GetXaxis()
   hists = []
   for i,tree in enumerate(trees):
-    h = h0.Clone("hist_%d" % i)
+    h = ROOT.TProfile ("hist_%d" % i, tree.GetTitle(), h0.GetNbinsX(), 0.0, float(h0.GetNbinsX()))
     hists.append(h)
-    h.Reset("M")
+    for bin in range(1,h0.GetNbinsX()+1):
+      h.GetXaxis().SetBinLabel (bin, ax.GetBinLabel(bin))
+    h.SetStats(0)
+    h.SetTitle(";;ns / double")
     h.SetMinimum(0.0)
     h.SetBarWidth(w)
+    h.SetMarkerSize (min (1.0, 3.5*w))
     h.SetBarOffset(0.3 + 1.3*w*i)
     h.SetFillColor(cols[i%len(cols)])
-    h.SetMarkerSize (min (1.0, 3.5*w))
-    hn = h.Clone("nent_%d" % i)
     for e in tree:
       nval = e.entries * e.branches * e.elements
       bin = ax.FindBin(e.label)
@@ -71,10 +83,7 @@ def process ():
       if bin < 0:
         print("no bin",e.label)
         continue
-      h.AddBinContent (bin, 1000000.0 * e.ms / float(nval))
-      hn.AddBinContent (bin)
-    h.Divide(hn)
-    del hn
+      h.Fill (e.label, 1000000.0 * e.ms / float(nval))
   if legends:
     leg = ROOT.TLegend (0.8,0.85,0.9,0.9)
     leg.SetFillStyle(0)
