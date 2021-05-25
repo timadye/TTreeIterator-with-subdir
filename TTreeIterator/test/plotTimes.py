27ecf327d5e91953f8706cecb1a16ad2d8c57420
Tim Adye <Tim.Adye@cern.ch>
2021-05-25 18:13:03 +0100

    print times and errors

--- TTreeIterator/test/plotTimes.py
+++ TTreeIterator/test/plotTimes.py
@@ -36,7 +36,8 @@ def process ():
       continue
     if not add:
       add=True
-      tree = ROOT.TTree ("times_%d" % i, f)
+      leg = legends[i] if legends and i<len(legends) else f
+      tree = ROOT.TTree ("times_%d" % i, leg)
       trees.append(tree)
       n = tree.ReadFile(f,"",",")
     else:
@@ -48,28 +49,31 @@ def process ():
       n = tree.ReadStream(fs,"",",")
       del fs
     if not n: return 1
-    print ("read %d entries from file %s into tree '%s'" % (n, f, tree.GetName()))
+    print ("read %d entries from file %s into tree '%s' with legend '%s'" % (n, f, tree.GetName(), tree.GetTitle()))
   nt = len(trees)
   sf = 1.0 / float(nt)
   w = min (0.45, 0.55*sf)
   ROOT.gStyle.SetPaintTextFormat(".0f")
   ROOT.gStyle.SetTickLength(0.0)
+  ROOT.gStyle.SetErrorX(0)
+  ROOT.gStyle.SetOptTitle(0)
   tree0 = trees[0]
   tree0.Draw("label","","goff")
   h0 = tree0.GetHistogram()
   if not h0:
     print ("element 'label' not found in '%s'" % tree0.GetName())
     return 2
-  h0.Reset("M")
-  ax = h0.GetXaxis()
+  ax0 = h0.GetXaxis()
+  labels = [(i,ax0.GetBinLabel(i)) for i in range(1,ax0.GetNbins()+1)]
+
   hists = []
   for i,tree in enumerate(trees):
-    h = ROOT.TProfile ("hist_%d" % i, tree.GetTitle(), h0.GetNbinsX(), 0.0, float(h0.GetNbinsX()))
+    h = ROOT.TProfile ("hist_%d" % i, "%s;;ns / double" % tree.GetTitle(), len(labels), 0.0, float(len(labels)))
     hists.append(h)
-    for bin in range(1,h0.GetNbinsX()+1):
-      h.GetXaxis().SetBinLabel (bin, ax.GetBinLabel(bin))
+    ax = h.GetXaxis()
+    for b,label in labels:
+      ax.SetBinLabel (b, label)
     h.SetStats(0)
-    h.SetTitle(";;ns / double")
     h.SetMinimum(0.0)
     h.SetBarWidth(w)
     h.SetMarkerSize (min (1.0, 3.5*w))
@@ -77,17 +81,20 @@ def process ():
     h.SetFillColor(cols[i%len(cols)])
     for e in tree:
       nval = e.entries * e.branches * e.elements
-      bin = ax.FindBin(e.label)
-      print ("%s:%d %s %s %-20s %s %-10s %1s %d %d %d %5g %5g (%d)" %
-             (tree.GetName(), bin, e.time, e.host, e.label, e.testcase, e.test, e.fill, e.entries, e.branches, e.elements, e.ms, e.cpu, nval))
-      if bin < 0:
+      print ("%s %s %s %-20s %s %-10s %1s %d %d %d %5g %5g (%d)" %
+             (tree.GetTitle(), e.time, e.host, e.label, e.testcase, e.test, e.fill, e.entries, e.branches, e.elements, e.ms, e.cpu, nval))
+      if ax.FindBin(e.label) < 0:
         print("no bin",e.label)
         continue
       h.Fill (e.label, 1000000.0 * e.ms / float(nval))
+
   if legends:
     leg = ROOT.TLegend (0.8,0.85,0.9,0.9)
     leg.SetFillStyle(0)
   for i,h in enumerate(hists):
+    ax = h.GetXaxis()
+    for b in range(1,h.GetNbinsX()+1):
+      print ("%-10s %-20s %5.1f%s ns/double" % (h.GetTitle(), ax.GetBinLabel(b), h.GetBinContent(b), (" +/- %3.1f" % h.GetBinError(b)) if nt>=2 else ""))
     if not i: h.Draw("bar text0")
     else:     h.Draw("bar text0 same")
     if legends and i<len(legends): leg.AddEntry(h,legends[i],"f")
